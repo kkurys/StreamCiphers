8b00555f299327ae1f3b4df707826b02c2f45444
mowerczuk <maciej.owerczuk@gmail.com>
2017-03-18 21:31:03 +0100

    some wild and crazy changes

--- StreamCiphers_Logic/AutokeyCiphertext.cs
+++ StreamCiphers_Logic/AutokeyCiphertext.cs
@@ -9,82 +9,82 @@ namespace StreamCiphers_Logic
 {
     public class AutokeyCiphertext
     {
-        List<int> seed;
-        List<int> f;
-        List<int> x;
-        List<int> y;
+        public List<int> Seed { get; set; }
+        public List<int> F { get; set; }
+        public List<int> X { get; set; }
+        public List<int> Y { get; set; }
 
         byte[] fileBytes;
 
         public AutokeyCiphertext()
         {
-            seed = new List<int>();
-            f = new List<int>();
-            x = new List<int>();
-            y = new List<int>();
+            Seed = new List<int>();
+            F = new List<int>();
+            X = new List<int>();
+            Y = new List<int>();
         }
 
         public AutokeyCiphertext(string fileName)
         {
             // czytanie z pliku !!
             fileBytes = File.ReadAllBytes(fileName);
-            seed = new List<int>();
-            f = new List<int>();
-            x = new List<int>();
-            y = new List<int>();
+            Seed = new List<int>();
+            F = new List<int>();
+            X = new List<int>();
+            Y = new List<int>();
         }
 
         public void Init(string argSeed, string argFunction, string argStream)
         {
             for (int i = 0; i < argSeed.Count(); i++)
             {
-                seed.Add(int.Parse(argSeed[i].ToString()));
-                y.Add(0);
+                Seed.Add(int.Parse(argSeed[i].ToString()));
             }
 
             for (int i = 0; i < 4; i++)
             {
-                f.Add(int.Parse(argFunction[i].ToString()));
+                F.Add(int.Parse(argFunction[i].ToString()));
             }
 
             for (int i = 0; i < 4; i++)
             {
-                x.Add(int.Parse(argStream[i].ToString()));
+                X.Add(int.Parse(argStream[i].ToString()));
             }
         }
 
         public List<int> Encrypt()
         {
 
-            for (int i = 0; i < seed.Count(); i++)
+            for (int i = 0; i < Seed.Count(); i++)
             {
-                for (int j = 0; j < f.Count(); j++)
+                Y.Add(0);
+                for (int j = 0; j < F.Count(); j++)
                 {
-                    if (f[j] == 1)
+                    if (F[j] == 1)
                     {
-                        y[i] += seed[j];
+                        Y[i] += Seed[j];
                     }
                 }
 
-                y[i] = (x[i] + y[i]) % 2;
+                Y[i] = (X[i] + Y[i]) % 2;
 
-                for (int j = seed.Count() - 1; j > 0; j--)
+                for (int j = Seed.Count() - 1; j > 0; j--)
                 {
-                    seed[j] = seed[j - 1];
+                    Seed[j] = Seed[j - 1];
                 }
-                seed[0] = y[i];
+                Seed[0] = Y[i];
             }
 
-            return y;
+            return Y;
         }
 
         public string GetEcnryptedString()
         {
             string result = "";
 
-            for (int i = 0; i < y.Count(); i++)
+            for (int i = 0; i < Y.Count(); i++)
             {
-                result += y[i];
+                result += Y[i];
             }
 
             return result;
--- StreamCiphers_Tests/AutokeyCiphertext_Tests.cs
+++ StreamCiphers_Tests/AutokeyCiphertext_Tests.cs
@@ -8,19 +8,26 @@ using StreamCiphers_Logic;
 
 namespace StreamCiphers_Tests
 {
+    [TestClass]
     class AutokeyCiphertext_Tests
     {
-        /*
         [TestMethod]
         public void AutokeyCiphertext_Logic_Initialization()
         {
             AutokeyCiphertext autokey = new AutokeyCiphertext();
 
             autokey.Init("1011", "1001", "1100");
-            Assert.AreEqual("1011", lfsr.Seed);
-            Assert.AreEqual("1101", lfsr.Polynomial);
+            List<int> checkSeed = new List<int>();
+            checkSeed.Add(1);
+            checkSeed.Add(0);
+            checkSeed.Add(1);
+            checkSeed.Add(1);
+            Assert.AreEqual(checkSeed, autokey.Seed);
+            //Assert.AreEqual("1101", lfsr.Polynomial);
 
         }
+
+        /*
         [TestMethod]
         public void AutokeyCiphertext_Operations_Work()
         {
