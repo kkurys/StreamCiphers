b7235322ec5d6c5e881a7266f387a3bb97ebf777
Kamil Kuryś <kkurys@10g.pl>
2017-03-18 16:46:41 +0100

    written some logic

--- StreamCiphers_Logic/LFSR.cs
+++ StreamCiphers_Logic/LFSR.cs
@@ -1,7 +1,40 @@
-﻿namespace StreamCiphers_Logic
+﻿using System;
+
+namespace StreamCiphers_Logic
 {
     public class LFSR
     {
+        public string Polynomial { get; set; }
+        public string Seed { get; set; }
 
+        public void Init(string _seed, string _polynomial)
+        {
+            Polynomial = _polynomial;
+            Seed = _seed;
+        }
+        public string GetOutput()
+        {
+            string result = "";
+            string lfsr = Seed;
+            for (int i = 0; i < Seed.Length; i++)
+            {
+                int lfsrInt = Convert.ToInt32(lfsr, 2);
+                int bit = lfsrInt;
+
+                for (int j = 1; j < Polynomial.Length; j++)
+                {
+                    if (Polynomial[j] == '1')
+                    {
+                        bit = bit ^ (lfsrInt >> (j - 1));
+                    }
+                }
+                bit = bit & 1;
+                lfsrInt = (lfsrInt >> 1) | (bit << (lfsr.Length - 1));
+                lfsr = Convert.ToString(lfsrInt, 2);
+                result += bit;
+            }
+
+            return result;
+        }
     }
 }
--- StreamCiphers_Tests/LFSR_Tests.cs
+++ StreamCiphers_Tests/LFSR_Tests.cs
@@ -1,13 +1,39 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using StreamCiphers_Logic;
 namespace StreamCiphers_Tests
 {
     [TestClass]
     public class LFSR_Tests
     {
         [TestMethod]
-        public void LFSR_Logic()
+        public void LFSR_Logic_Initialization()
         {
+            LFSR lfsr = new LFSR();
+
+            lfsr.Init("1011", "1101");
+            Assert.AreEqual("1011", lfsr.Seed);
+            Assert.AreEqual("1101", lfsr.Polynomial);
+
+        }
+        [TestMethod]
+        public void Math_Operations_Work()
+        {
+            int seed = 11;
+            Assert.AreEqual(11, seed);
+            // seed = 1011, polynomial = 1101
+            int bit = ((seed >> 0) ^ (seed >> 1) ^ (seed >> 3)) & 1;
+            Assert.AreEqual(1, bit);
+
+        }
+        [TestMethod]
+        public void LFSR_Logic_Engine_Works()
+        {
+            LFSR lfsr = new LFSR();
+
+            lfsr.Init("1011", "11001");
+            string result = lfsr.GetOutput();
+
+            Assert.AreEqual("0110", result);
         }
     }
 }
