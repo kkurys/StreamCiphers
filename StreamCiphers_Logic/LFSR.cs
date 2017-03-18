b764f4331c1efc966546db29df1592d8337bc224
Kamil Kuryś <kkurys@10g.pl>
2017-03-18 21:56:13 +0100

    lfsr seems to be working

--- StreamCiphers_Logic/LFSR.cs
+++ StreamCiphers_Logic/LFSR.cs
@@ -12,26 +12,52 @@ namespace StreamCiphers_Logic
             Polynomial = _polynomial;
             Seed = _seed;
         }
+        public int XORBits(int _base)
+        {
+            int bit = _base;
+            for (int j = 1; j < Polynomial.Length; j++)
+            {
+                if (Polynomial[j] == '1')
+                {
+                    bit = bit ^ (_base >> j);
+                }
+            }
+            string bitString = Convert.ToString(bit, 2);
+           // bitString = bitString.PadLeft(Seed.Length, '0');
+            if (bitString.LastIndexOf('0') != -1) return 0;
+            return 1;
+        }
+        public int ReplaceFirstBit(int _base, int bit)
+        {
+            string _baseString = Convert.ToString(_base, 2);
+
+            _baseString = _baseString.PadLeft(Seed.Length, '0');
+            _baseString = bit.ToString() + _baseString.Substring(1);
+
+            return Convert.ToInt32(_baseString, 2);
+        }
+        public int Shift(int _base)
+        {
+            return (_base >> 1);
+        }
         public string GetOutput()
         {
             string result = "";
-            string lfsr = Seed;
+            string ciphered = Seed;
             for (int i = 0; i < Seed.Length; i++)
             {
-                int lfsrInt = Convert.ToInt32(lfsr, 2);
-                int bit = lfsrInt;
+                int cipheredInt = Convert.ToInt32(ciphered, 2);
 
-                for (int j = 1; j < Polynomial.Length; j++)
-                {
-                    if (Polynomial[j] == '1')
-                    {
-                        bit = bit ^ (lfsrInt >> (j - 1));
-                    }
-                }
-                bit = bit & 1;
-                lfsrInt = (lfsrInt >> 1) | (bit << (lfsr.Length - 1));
-                lfsr = Convert.ToString(lfsrInt, 2);
-                result += bit;
+                int bit = XORBits(cipheredInt);
+
+                cipheredInt = Shift(cipheredInt);
+
+                cipheredInt = ReplaceFirstBit(cipheredInt, bit);
+
+                ciphered = Convert.ToString(cipheredInt, 2).PadLeft(Seed.Length, '0');
+                if ( i == 3)
+                    return ciphered;
+                result += ciphered[0];
             }
 
             return result;
--- StreamCiphers_Tests/LFSR_Tests.cs
+++ StreamCiphers_Tests/LFSR_Tests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StreamCiphers_Logic;
+
 namespace StreamCiphers_Tests
 {
     [TestClass]
@@ -26,11 +27,45 @@ namespace StreamCiphers_Tests
 
         }
         [TestMethod]
+        public void LFSR_Shift_Works()
+        {
+            LFSR lfsr = new LFSR();
+
+            lfsr.Init("1011", "11001");
+
+            Assert.AreEqual(2, lfsr.Shift(4));
+        }
+        [TestMethod]
+        public void LFSR_Logic_XOR_Works()
+        {
+            LFSR lfsr = new LFSR();
+
+            lfsr.Init("1011", "11001");
+
+            Assert.AreEqual(0, lfsr.XORBits(11));
+            Assert.AreEqual(1, lfsr.XORBits(5));
+            Assert.AreEqual(1, lfsr.XORBits(10));
+            Assert.AreEqual(0, lfsr.XORBits(13));
+        }
+        [TestMethod]
+        public void LSFR_Logic_Bit_Replacing_Works()
+        {
+            LFSR lfsr = new LFSR();
+
+            lfsr.Init("1011", "11001");
+
+            Assert.AreEqual(15, lfsr.ReplaceFirstBit(7, 1));
+            Assert.AreEqual(0, lfsr.ReplaceFirstBit(8, 0));
+            Assert.AreEqual(3, lfsr.ReplaceFirstBit(11, 0));
+
+        }
+        [TestMethod]
         public void LFSR_Logic_Engine_Works()
         {
             LFSR lfsr = new LFSR();
 
             lfsr.Init("1011", "11001");
+
             string result = lfsr.GetOutput();
 
             Assert.AreEqual("0110", result);
